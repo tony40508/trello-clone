65e360e5bb1b298af86a79c8d2677d7ad00c658a
Takeu Chen <tony40508@gmail.com>
2018-06-09 01:45:53 +0800

    💥 add apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createNetworkInterface
+} from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Routes from './routes';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
