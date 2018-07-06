28a86c686044ecc1bc1f3efb46d3589814030b6d
Takeu Chen <tony40508@gmail.com>
2018-07-06 16:53:15 +0800

    🚧 update client

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,49 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {
+//   ApolloProvider,
+//   ApolloClient,
+//   createNetworkInterface
+// } from 'react-apollo';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// import Routes from './routes';
+
+// injectTapEventPlugin();
+
+// const networkInterface = createNetworkInterface({
+//   uri: 'http://localhost:3030/graphql'
+// });
+
+// networkInterface.use([
+//   {
+//     applyMiddleware(req, next) {
+//       if (!req.options.headers) {
+//         req.options.headers = {};
+//       }
+//       req.options.headers['x-token'] = localStorage.getItem('token');
+//       req.options.headers['x-refresh-token'] = localStorage.getItem(
+//         'refreshToken'
+//       );
+//       next();
+//     }
+//   }
+// ]);
+
+// const client = new ApolloClient({
+//   networkInterface
+// });
+
+// const App = (
+//   <MuiThemeProvider>
+//     <ApolloProvider client={client}>
+//       <Routes />
+//     </ApolloProvider>
+//   </MuiThemeProvider>
+// );
+
+// ReactDOM.render(App, document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
@@ -5,8 +51,9 @@ import {
   ApolloClient,
   createNetworkInterface
 } from 'react-apollo';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import Routes from './routes';
 
 injectTapEventPlugin();
@@ -14,6 +61,21 @@ injectTapEventPlugin();
 const networkInterface = createNetworkInterface({
   uri: 'http://localhost:3030/graphql'
 });
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {};
+      }
+      req.options.headers['x-token'] = localStorage.getItem('token');
+      req.options.headers['x-refresh-token'] = localStorage.getItem(
+        'refreshToken'
+      );
+      next();
+    }
+  }
+]);
 
 const client = new ApolloClient({
   networkInterface
